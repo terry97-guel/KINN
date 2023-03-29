74c4960a1664eb5dd2a59d5007edf9f1efe4fc0e
terry <taerimyoon@korea.ac.kr>
2023-03-29 16:15:08 +0900

    change direction of  the position of finger dataset

---
+++ temp_data_convert.py
@@ -0,0 +1,53 @@
+
+# convert data for temporary purposes
+# %%
+
+import json
+from pathlib import Path
+
+datapath = Path("dataset/FINGERN.json")
+
+with open(datapath, "r") as f:
+    data = json.load(f)
+    
+print(data)
+
+# %%
+
+transformation_matrix = np.array(
+    [
+        [ 0,-1, 0],
+        [ 0, 0, 1],
+        [-1, 0, 0]
+    ]
+)
+
+import numpy as np
+
+for key in data.keys():
+    partial_data = data[key]['position'] 
+    partial_data
+    temp = np.array(partial_data)
+    transformed_partial_data = temp@transformation_matrix
+    data[key]['position']  = (transformed_partial_data).tolist()
+
+
+# %%
+from matplotlib import pyplot as plt
+
+for key in data.keys():
+    partial_data = np.array(data[key]['position'])
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+
+    ax.scatter(partial_data[:,0,0], partial_data[:,0,1], partial_data[:,0,2])
+    plt.show()
+    
+
+# %%
+
+# save the result_json as "FINGER.json"
+with open("dataset/FINGER.json", "w") as f:
+    json.dump(data, f)
+    
+# %%
