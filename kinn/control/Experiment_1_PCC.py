a03c362fc51debc1e3e8d1978f907b7e7fa17d8b
terry <taerimyoon@korea.ac.kr>
2023-11-25 18:09:20 +0900

    pcc ik

--- kinn/control/Experiment_1_PCC.py
+++ kinn/control/Experiment_1_PCC.py
@@ -1,4 +1,9 @@
 # %%
+TRAJ_SIZE = 'small' # 'large' or 'small'
+from kinn.utils.tools import set_seed
+set_seed(0)
+# %%
+# dataset = "ELASTICA" 
 dataset = "FINGER" 
 
 from pathlib import Path
@@ -13,14 +18,14 @@ BASEDIR
 
 # %%
 from kinn.utils.dataloader import get_dataset, Sampler
-train_dataset,val_dataset,test_dataset,ext_dataset  = get_dataset(f"../dataset/{dataset}.json", 1.0)
+train_dataset,val_dataset,test_dataset,ext_dataset  = get_dataset(f"../../dataset/{dataset}.json", 1.0)
 
 # %%
 from kinn.utils.args import read_ARGS
 configs = f"PRIMNET/{dataset}.py"
 
 
-args = read_ARGS((BASEDIR/'configs'/configs).absolute())
+args = read_ARGS((BASEDIR/'../configs'/configs).absolute())
 args
 
 # %%
@@ -149,7 +154,7 @@ def predict(LR, motor_control):
     
     l1,l3,l4,l2 = Lengths[:,0],Lengths[:,1],Lengths[:,2],Lengths[:,3]
 
-    phi = torch.atan2((l4-l2),(l3-l1))
+    phi = -torch.atan2((l4-l2),(l3-l1))
     theta = torch.sqrt((l3-l1)**2 + (l4-l2)**2) / (2*d)
     l = (l1+l2+l3+l4)/4
     radius = l/theta
@@ -167,10 +172,11 @@ def predict(LR, motor_control):
     base_position = torch.stack([x,y,z]).reshape(-1,1)
     
     # assert pred_position.shape[1:] == base_position.shape
-    # pred_position = pred_position + base_position
+    pred_position = pred_position + base_position
     return pred_position 
 
 
+# %%
 h_lbfgs = []
 
 for i in range(100):
@@ -193,12 +199,6 @@ plt.legend()
 plt.show()
 
 # %%
-LR
-
-# %%
-
-
-# %%
 pred_position = predict(LR, motor_control)
 position_loss =  p_loss_fn(pred_position, target_position[:,-1])
 objective = torch.mean(position_loss)
@@ -218,12 +218,105 @@ for sampler in [test_sampler, ext_sampler]:
 
         total_loss = torch.mean(position_loss)
         print(float(total_loss))
-        
+# %%
+# Solve IK
+
+if TRAJ_SIZE.lower()=="small":
+    target_trajectory = torch.FloatTensor(
+        [[0,0,0],[-0.5,-0.5,0],[0.5,-0.5,0],[0.5,0.5,0],[-0.5,0.5,0],[-0.5,-0.5,0]]
+        ) * 30/1000
+else:
+    target_trajectory = torch.FloatTensor(
+        [[0,0,0],[-0.5,-0.5,0],[0.5,-0.5,0],[0.5,0.5,0],[-0.5,0.5,0],[-0.5,-0.5,0]]
+        ) * 60/1000
 
 # %%
+import numpy as np
+def interpolate(a, b, n_points):
+    interp_points = torch.linspace(0, 1, n_points+2)[:]
+    output = (1 - interp_points)[:, None] * a + interp_points[:, None] * b
+    return output
 
 
 # %%
+from functools import partial
+motor_control_list = []
+position_list = []
+
+scale_rate = 2000
+
+for i in range(len(target_trajectory)):
+    motor_actuation = torch.FloatTensor([[1, 0]]).requires_grad_(True)
+    target_position_tensor = target_trajectory[i]
+
+    def loss_pred_position(motor_control):
+        pred_position = predict(LR, motor_control)
+        return p_loss_fn(pred_position.flatten()[:2], target_position_tensor.flatten()[:2])
+
+    optimizer = optim.LBFGS([motor_actuation],
+                            history_size=10,
+                            max_iter=4,
+                            line_search_fn="strong_wolfe")
+
+    # optimizer = optim.LBFGS([motor_actuation],
+    #                         history_size=10,
+    #                         max_iter=4)
+
+    def closure(motor_actuation):
+        optimizer.zero_grad()
+        loss = loss_pred_position(motor_actuation*scale_rate )  # Pass motor_control to loss_pred_position
+        loss.backward()
+        return loss
+
+    loss_list = []
+    for i in range(100):
+        optimizer.step(partial(closure, motor_actuation))  # Pass the closure function
+        # print loss
+        loss = closure(motor_actuation)
+        loss_list.append(loss.item())
+    
+    plt.plot(loss_list)
+
+    motor_control = motor_actuation*scale_rate
+    # Capture results after optimization
+    with torch.no_grad():
+        pred_position = predict(LR, motor_control)
+        motor_control_list.append(motor_control.detach().numpy().tolist())
+        position_list.append(pred_position.detach().numpy().tolist())
+
+        print("here")
+# %%
+position_array = np.array(position_list).reshape(-1,3)[:,:2]
+
+plt.plot(position_array[:,0], position_array[:,1])
+plt.plot(target_trajectory[:,0], target_trajectory[:,1], alpha=0.5)
+plt.axis('equal')
+
+# %%
+motor_control_list
+# %%
+
+# %%
+motor_control = torch.FloatTensor([[1000, 0]])
+pred_position = predict(LR, motor_control)
+pred_position
 
 
+model = 'PCC'
 
+if TRAJ_SIZE.lower() == 'small':
+    name = "small_square"
+else:
+    name = "large_square"
+    
+path = Path(f"planned_traj/{name}/{model}_plan.npz")
+path.parent.mkdir(parents=True, exist_ok=True)
+np.savez(path,
+          actuation=np.array(motor_control_list),
+            target_trajectory=target_trajectory)
+
+data = np.load(path)
+# %%
+data['actuation'],
+data['target_trajectory']
+# %%
